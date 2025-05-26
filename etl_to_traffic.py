5112116978646ce0f8526502cab2f3366206c2b4
liam <ggambuk@gmail.com>
2025-05-26 22:50:50 +0900

    v1.1.0 에러 에외처리 로그파일생성

---
+++ etl_to_traffic.py
@@ -0,0 +1,188 @@
+# ETL 
+# 클릭하우스 DB에서 데이터를 추출
+# 웹 패킷을 전송하기 위해 전처리 (피클 or CSV 파일로 변환 or 메모리에서 바로 전송)
+
+import pandas as pd
+import requests
+import pickle
+import json
+import clickhouse_connect
+import os
+from requests.exceptions import ConnectTimeout
+from clickhouse_connect.driver.exceptions import (
+    ClickHouseError,
+    OperationalError,
+    ProgrammingError
+)
+
+class ClickHouseETL:
+    def __init__(self, host: str, tg_ip: str, attack: str, limit: int):
+        self.host = host
+        self.df = None   
+        self.total = 0
+        self.count = 0
+        self.error_count = 0
+        self.tg_ip = tg_ip
+        self.attack = attack
+        self.limit = limit
+
+
+    def extract(self):
+        print("[i] loading data from ClickHouse...")
+        
+        try:
+            client = clickhouse_connect.get_client(host=self.host, database="DTIAI")
+            query = f"SELECT * FROM http_packet_temp WHERE label = '{self.attack}' AND dest_port IN (8123, 8181) ORDER BY start_date DESC LIMIT {self.limit}"
+            print(f"[i] Query: {query}")
+            self.df = client.query_df(query)
+
+            print(f"[i] Loaded {len(self.df)}개 rows from ClickHouse.")
+            self.total = len(self.df)
+            self.__transform_to_parquet() # Save to parquet file
+
+        except (OperationalError, ProgrammingError, ValueError, Exception) as e:
+            db_error_log_path = "./errors/data_load_failed_log.txt" 
+            with open(db_error_log_path, "a", encoding="utf-8") as f:
+                failed = f"[!] DB 쿼리 실패 또는 데이터 없음: {e}"
+                print(failed)        # 콘솔 출력
+                f.write(failed + "\n")  # 파일에 한 줄로 저장
+            try:
+                # 예: 'backup_data.parquet' 파일에서 대체 데이터 로드
+                self.df = pd.read_parquet(f"./data/{self.attack}.parquet")
+                self.total = len(self.df)
+                print(f"[i] Parquet 파일에서 대체 데이터 {self.total}개 로드 완료.")
+            except Exception as pe:
+                db_error_log_path = "./errors/data_load_failed_log.txt"  # 또는 Path 객체도 가능
+                with open(db_error_log_path, "a", encoding="utf-8") as f:
+                    failed = f"[e] Parquet 로드 실패: {pe}"
+                    print(failed)        # 콘솔 출력
+                    f.write(failed + "\n")  # 파일에 한 줄로 저장
+                    
+                self.df = pd.DataFrame()  # 완전 실패 시 빈 DataFrame 처리
+        
+    def __transform_to_parquet(self):
+        path = f"./data/{self.attack}.parquet"
+        print(f"[T] Saving to parquet: {path}")
+        df = self.df.astype({col: str for col in self.df.select_dtypes(include='object').columns})
+        df.to_parquet(path)
+
+
+    def __parse_raw_http_header(self, raw_header: str) -> dict:
+        headers = {}
+        lines = raw_header.strip().split("\\r\\n")  # 문자열 안에 \r\n이 있음
+        
+        # 첫 줄은 요청 라인 (예: "POST /path HTTP/1.1")이므로 제외하고 헤더 라인만 처리
+        for line in lines[1:]:  
+            # 헤더 라인은 일반적으로 "Key: Value" 형식이므로 ': ' 포함 여부 확인
+            if ": " in line:
+                # 첫 번째 ': ' 기준으로 key와 value 분리 (':'가 value에 포함된 경우도 있으므로 maxsplit=1)
+                key, value = line.split(": ", 1)
+
+                # key와 value 양쪽 공백 제거하여 딕셔너리에 저장
+                # 공백을 제거하지 않으면 잘못된 헤더값(예: ' Not(...)')으로 에러 발생 가능
+                headers[key.strip()] = value.strip()
+
+        # 최종적으로 파싱된 헤더 딕셔너리 반환
+        return headers
+
+                    
+    def __extract_port_from_raw_header(self, host: str) -> str:
+         # "Host: 211.115.206.18:80" → ["Host", " 211.115.206.18", "80"]
+        parts = host.split(":", 1)  # 최대 1번만 나누어서 세 부분으로 나눔
+        if len(parts) == 2:
+            # 세 번째 부분은 포트 번호이므로 공백 제거 후 반환
+            return parts[1].strip()
+        
+        # "Host:" 줄을 못 찾거나 포트가 없는 경우 None 반환
+        return None
+
+
+
+    def send_requests(self, progress_callback=None, stop: bool = False):
+        print("[L] Sending HTTP requests..................")
+        for idx, row in self.df.iterrows():
+            if stop or self.total == self.count:
+                print("Stopping the request sending process.")
+                break
+            if self.count == self.error_count and self.error_count > 5:
+                failed = f"[!]: 요청에 문제가 있습니다 에러 로그를 확인하세요."
+                progress_callback(self.count, failed, self.error_count)
+                print(failed)
+                break
+            try:
+                uuid = row['id']
+                pre_url = row['url']
+                method = row.get('method', 'GET').upper()
+                headers_str = row.get('request_header', '{}')
+                headers = self.__parse_raw_http_header(headers_str)
+                print(f"[{idx}] Headers: {headers}")
+                dest_port = row['dest_port']
+                
+                # 포트 번호가 없을 경우 처리
+                # 포트 번호가 없으면 8181으로 설정
+                port = int(dest_port) if dest_port else 8181
+
+
+                # 받는 서버에서 바는 포트 번호가 열려 있지안으면  아래같은 에러 생기므로 8123같은 포트에 해당하는 앱 배포해 놔야한다.
+                # Failed to establish a new connection: [WinError 10061] 대상 컴퓨터에서 연결을 거부했으므로 연결하지 못했습니다'
+                url = f"http://{self.tg_ip}:{port}{pre_url}"
+                print(f'idx = {idx}, URL = {url}')
+                body = row.get('request_body', None)
+                # print(f'바디 = {body}')
+
+
+                if method == 'POST':
+                    print(f"[{idx}] Sending POST request to {url}")
+                    res = requests.post(url, headers=headers, data=body)
+                    success = f"[{idx}] [{method}] {url} → {res.status_code}"
+                    print(success)
+                    self.count += 1
+                    if progress_callback:
+                        progress_callback(self.count, success, self.error_count)
+
+                else:
+                    print(f"[{idx}] Sending GET request to {url}")
+                    res = requests.get(url, headers=headers)
+                    success = f"[{idx}] [{method}] {url} → {res.status_code}"
+                    print(success)
+                    self.count += 1
+                    if progress_callback:
+                        progress_callback(self.count, success, self.error_count)
+                    
+            except requests.exceptions.RequestException as e:
+                log_path = "./errors/Request_failed_log.txt"  # 또는 Path 객체도 가능
+                failed = f"[!]기타 요청 예외: [{idx}] transaction_id : {uuid}\n AND Error: \n{e}"
+                with open(log_path, "a", encoding="utf-8") as f:
+                    print(failed)        # 콘솔 출력
+                    f.write(failed + "\n")  # 파일에 한 줄로 저장
+                    
+                self.error_count += 1
+                self.count += 1
+                if progress_callback:
+                   progress_callback(self.count, failed, self.error_count)
+                   
+            except requests.exceptions.Timeout as e:
+                failed = f"[!]요청 타임아웃: failed [{idx}] transaction_id : {uuid}\n AND Error: \n{e}"
+                log_path = "./errors/failed_log.txt"  # 또는 Path 객체도 가능
+
+                with open(log_path, "a", encoding="utf-8") as f:
+                    print(failed)        # 콘솔 출력
+                    f.write(failed + "\n")  # 파일에 한 줄로 저장
+                
+                self.error_count += 1
+                self.count += 1
+                if progress_callback:
+                   progress_callback(self.count, failed, self.error_count)
+                   
+            except ConnectTimeout as e:
+                failed = f"[!]연결 타임아웃 발생: {e}"
+                log_path = "./errors/timeout_log.txt"  # 또는 Path 객체도 가능
+                with open(log_path, "a", encoding="utf-8") as f:
+                    failed = f"failed [{idx}] transaction_id : {uuid} \n AND timeout Error: \n{e}"
+                    print(failed)        # 콘솔 출력
+                    f.write(failed + "\n")  # 파일에 한 줄로 저장
+                break
+                
+                
+
+        
--- traffic_generator.py
+++ traffic_generator.py
@@ -2,22 +2,29 @@ import tkinter as tk
 import subprocess
 from tkinter import ttk 
 import re
+from etl_to_traffic import *
+from threading import Thread
 
 root = tk.Tk()
 root.title("traffic_generator")
 root.geometry("900x400")
 
+    
 running = False
 complete = False
 user_attack_input = ""
 user_ip_input = ""
-# 패킷 개수 받아오는 로직 TODO
-total_packet = 100 # = packet_total_num()
+
+total_packet = 10 #15000 # 여기서 리터럴로 개수설정 아차피 시험 절차서에 명시된 개수로 해야된다.
+# 1초 천개 이상합격
+# 대규모 네트워크 환경에서 수집 된 데이터를 시스템이, 
+# 15분간 처리하는 데이터양을 확인 100만개 이상 이면 합격
 
 count = 0
 
 placeholder_ip = "IP 주소를 입력하세요"
 placeholder_attack = "공격 방식을 입력하세요"
+placeholder_num = "전송할 데이터수를 입력하세요"
 
 # 정규식 기반: 입력 중에도 허용 가능한 IP 문자열 검사
 def validate_ip_partial(text):
@@ -35,13 +42,8 @@ def validate_ip_partial(text):
 
 
 vcmd = (root.register(validate_ip_partial), "%P")
-# --- 위젯들 미리 선언 (한 번만 생성) ---
-# target_input = tk.Entry(root, font=("Helvetica", 16))
 
-target_input = tk.Entry(root,
-                        font=("Helvetica", 16),
-                        validate="key",
-                        validatecommand=vcmd)
+# --- 위젯들 미리 선언 (한 번만 생성) ---
 
 
 target_input = tk.Entry(root, font=("Helvetica", 16),
@@ -58,97 +60,99 @@ attack_input.insert(0, placeholder_attack)
 attack_input.bind('<FocusIn>', lambda e: (attack_input.delete(0, 'end'), attack_input.config(fg='black')) if attack_input.get() == placeholder_attack else None)
 attack_input.bind('<FocusOut>', lambda e: attack_input.insert(0, placeholder_attack) if attack_input.get() == '' else None)
 
+amount_input = tk.Entry(root, font=("Helvetica", 16),
+                        validate="key", 
+                        fg='grey')
+amount_input.insert(0, placeholder_num)
+amount_input.bind('<FocusIn>', lambda e: (amount_input.delete(0, 'end'), amount_input.config(fg='black')) if amount_input.get() == placeholder_num else None)
+amount_input.bind('<FocusOut>', lambda e: amount_input.insert(0, placeholder_num) if amount_input.get() == '' else None)
+
 target_ip = tk.Label(root, text="", font=("Helvetica", 16))
 attack_name = tk.Label(root, text="", font=("Helvetica", 16))
 
 packet_num = tk.Label(root, text="패킷 0개", font=("Helvetica", 20))
-progress = ttk.Progressbar(root, orient="horizontal", length=300, mode="determinate")
+progress = ttk.Progressbar(root, orient="horizontal", length=250, mode="determinate")
+packet_status = tk.Label(root, text="", font=("Helvetica", 8))
 # btn_pogr = tk.Button(root, text="시작")
 btn_ip_show = tk.Button(root, text="확인")
 # 버튼 구성
 start_btn = tk.Button(root, text="▶ 시작")
 stop_btn = tk.Button(root, text="⏸ 중지")
-
-
-
+btn_refresh = tk.Button(root, text="재시작")
 # =======================================================================================
+
+
+
 
 # --- 함수: IP 입력 후 진행창 보여주기 ---
 def ip_input():
+    global etl, total_packet
     user_ip_input = target_input.get()
     user_attack_input = attack_input.get()
+    total_packet = int(amount_input.get())
     target_ip.config(text=f"Target IP: {user_ip_input}")
     attack_name.config(text=f"Attack Name: {user_attack_input}")
 
+    # 패킷 전송 함수 호출
+    etl = ClickHouseETL(host='211.115.206.18', tg_ip=user_ip_input, attack=user_attack_input, limit=total_packet)
+    etl.extract()
+    
     # 입력창 숨기기
     target_input.pack_forget()
     btn_ip_show.pack_forget()
     attack_input.pack_forget()
+    amount_input.pack_forget()
 
     # 진행화면 보여주기
     target_ip.pack()
     attack_name.pack()
     packet_num.pack(pady=40)
     progress.pack()
-    
-    # btn_pogr.pack(pady=10)
+    packet_status.pack(pady=10)
 
     # --- 버튼 배치 ---
     start_btn.pack(side="left", padx=20)
-    stop_btn.pack(side="right", padx=20)    
+    stop_btn.pack(side="right", padx=20)   
     
-# --- 함수: 진행 바 재시작 ---
-def reset_progress(count: int = 0):
-    progress['value'] = 0
-    packet_num.config(text=f'패킷 {count}개 전송 완료 (초기화됨)')
-    root.update_idletasks()
-
-
-# --- 함수: 진행 바 시작 ---
-# def start_progress():
-#     global count
-#     progress['maximum'] = 100
-#     for i in range(101):
-#         if count < 100:
-#             packet_num.config(text=f'패킷 {count} 개')
-#             count += 1
-#             progress['value'] = i
-#             root.update_idletasks()
-#             root.after(20)
-#         elif count == 100:
-#             count = 0
-#             # 완료 후 1초 뒤 초기화
-#             packet_num.config(text=f'패킷 {count} 개')
-#             root.after(500, reset_progress(count))
-
 
 # --- 진행 업데이트 함수 ---
-def update_progress():
-    global running, count, total_packet, complete
+def update_progress(count: int, result: str, error: int):
+    global running, total_packet, complete, user_ip_input
+    failed = "" 
+    # 실패한 패킷 수를 세는 로직
+    if result.startswith("[!]"):
+        failed = result
+        print(f"전송 실패 패킷수= {error}")
+        packet_num.config(text=f"패킷 {total_packet} 개중 {error}개 전송 실패")
+        packet_status.config(text=failed)
+        packet_status.config(fg="red")
     # 리턴 값으로  전송될 전체 패킷 개수
-    if running and count < total_packet:
-        #
-        # request 패킷전송 함수 로직 TODO
-        #
+    elif running and count < total_packet:
+        print(f"전송 시도 패킷수= {count}")
         packet_num.config(text=f"패킷 {total_packet} 개중 {count} 전송중")
+        packet_status.config(text=result)
+        packet_status.config(fg="green")
+        
         progress['value'] = count
-        count += 1
-        root.after(50, update_progress)
-        print(count)
-    elif count == 100:
+
+        progress.update_idletasks()
+    elif count == total_packet:
         complete = True
-        # 완료 후 0.5초 뒤 초기화
-        packet_num.config(text=f'패킷 {count} 개')
-        root.after(500, reset_progress(count))
-        count = 0
+        progress['value'] = progress['maximum']
+        progress.update_idletasks()
         running = False
+        success = count - error
+        packet_num.config(text=f"[결과] 패킷 {total_packet} 개중 {success} 전송성공 {error}개 전송실패")
+        packet_status.config(text=failed)
+        packet_status.config(fg="red")
+    
 
 # --- 시작 버튼 ---
 def start_progress():
     global running, complete
+    Thread(target=lambda: etl.send_requests(progress_callback=update_progress)).start()
     complete = False
     running = True
-    update_progress()
 
 # --- 중지 버튼 ---
 def stop_progress():
@@ -160,9 +164,10 @@ def stop_progress():
         packet_num.config(text=f'⏹️ 중지됨 현재 {total_packet}중 패킷 {count}개 전송')
     else:
         packet_num.config(text=f'⏹️ 중지됨 현재 {total_packet}중 패킷 {count}개 전송')
+        
+    etl.send_requests(progress_callback=update_progress, stop=True)
 
 # --- 버튼 과 함수 매칭 ---
-# btn_pogr.config(command=start_progress)
 
 start_btn.config(command=start_progress)
 
@@ -171,13 +176,13 @@ stop_btn.config(command=stop_progress)
 
 # --- 함수: 재시작 (초기 UI로 되돌리기) ---
 def refresh():
-    global count, user_attack_input, user_ip_input
+    global count, user_attack_input, user_ip_input, total_packet
     user_attack_input = ""
     user_ip_input = ""
+    total_packet = 0
     count = 0
     root.after(200, stop_progress())
 
-    # request 패킷전송 함수 초기화 로직 TODO
     # 진행 바 초기화
     progress['value'] = 0
     packet_num.config(text=f'패킷 {count}개 전송 완료 (초기화됨)')
@@ -186,6 +191,7 @@ def refresh():
     attack_name.pack_forget()
     packet_num.pack_forget()
     progress.pack_forget()
+    packet_status.pack_forget()
     # btn_pogr.pack_forget()
     start_btn.pack_forget()
     stop_btn.pack_forget()
@@ -193,10 +199,13 @@ def refresh():
 
     # 입력창 초기화 및 다시 보여주기
     target_input.delete(0, tk.END)
-    target_input.pack()
+    target_input.pack(padx=20, pady=20)
     
     attack_input.delete(0, tk.END)
-    attack_input.pack()
+    attack_input.pack(padx=20, pady=20)
+    amount_input.delete(0, tk.END)
+    amount_input.pack(padx=20, pady=20)
+    btn_ip_show.config(command=ip_input)
     btn_ip_show.pack()
    
     root.update_idletasks()
@@ -204,11 +213,11 @@ def refresh():
 # --- 초기 UI 구성 ---
 target_input.pack(padx=20, pady=20)
 attack_input.pack(padx=20, pady=20)
+amount_input.pack(padx=20, pady=20)
 btn_ip_show.config(command=ip_input)
 btn_ip_show.pack()
 
 # --- 재시작 버튼 (항상 오른쪽 하단) ---
-btn_refresh = tk.Button(root, text="재시작", command=refresh)
-btn_refresh.place(relx=1.0, rely=1.0, anchor="se")
-
+btn_refresh.config(command=refresh)
+btn_refresh.place(relx=1.0, rely=1.0, anchor="se") 
 root.mainloop()
